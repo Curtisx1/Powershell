8f48deb826b16c59fa1b1fcb0604f3e037e00616
Curtis Lemke <113563520+Curtisx1@users.noreply.github.com>
2023-09-12 17:17:55 -0500

    Add files via upload

---
+++ Place_O365_Shortcuts_desktop.ps1
@@ -0,0 +1,22 @@
+﻿# Get the path to the desktop
+$DesktopPath = [Environment]::GetFolderPath("Desktop")
+
+# Define the paths to the applications
+$OutlookPath = "C:\Program Files\Microsoft Office\root\Office16\OUTLOOK.EXE"
+$ExcelPath = "C:\Program Files\Microsoft Office\root\Office16\EXCEL.EXE"
+$WordPath = "C:\Program Files\Microsoft Office\root\Office16\WINWORD.EXE"
+
+# Create the shortcuts
+$WshShell = New-Object -ComObject WScript.Shell
+
+$Shortcut = $WshShell.CreateShortcut("$DesktopPath\Outlook.lnk")
+$Shortcut.TargetPath = $OutlookPath
+$Shortcut.Save()
+
+$Shortcut = $WshShell.CreateShortcut("$DesktopPath\Excel.lnk")
+$Shortcut.TargetPath = $ExcelPath
+$Shortcut.Save()
+
+$Shortcut = $WshShell.CreateShortcut("$DesktopPath\Word.lnk")
+$Shortcut.TargetPath = $WordPath
+$Shortcut.Save()
---
+++ get_aduser_emails.ps1
@@ -0,0 +1,41 @@
+﻿# Import the Active Directory module
+Import-Module ActiveDirectory
+
+# Import the CSV file
+$csv = Import-Csv -Path 'C:\install\emails.csv'
+
+# Create an array to hold the results
+$results = @()
+
+# Loop through each row in the CSV
+foreach ($row in $csv) {
+    # Get the email address from the row
+    $email = $row.Email
+
+    # Search for the user in Active Directory
+    $user = Get-ADUser -Filter "EmailAddress -eq '$email'" -Properties EmailAddress, DistinguishedName
+
+    # If the user was found
+    if ($user) {
+        # Get the OU from the user's DistinguishedName
+        $ou = ($user.DistinguishedName -split ',', 2)[1]
+
+        # Split the OU on the comma character and select the third part
+        $specificOU = ($ou -split ',')[2]
+
+        # Add the result to the results array
+        $results += New-Object PSObject -Property @{
+            'Email' = $email
+            'OU' = $specificOU
+        }
+    } else {
+        # Add the result to the results array
+        $results += New-Object PSObject -Property @{
+            'Email' = $email
+            'OU' = 'User not found in Active Directory'
+        }
+    }
+}
+
+# Export the results to a CSV file
+$results | Export-Csv -Path 'C:\install\branches.csv' -NoTypeInformation
